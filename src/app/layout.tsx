b612e67df22d9c1a5675853b88a0f90e01f47650
menna-adel-10 <menna10_adel@hotmail.com>
2024-04-16 23:04:38 +0200

    .

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import "./globals.css";
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+      <PrismicPreview repositoryName={repositoryName} />
+    </html>
+  );
+}
